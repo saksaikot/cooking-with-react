8e0e01fb49ef267dc9862ab4f82a91a97c1bafe3
saksaikot <saksaikot@gmail.com>
2021-06-03 23:36:16 +0600

    cleanup for recipe project

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,7 @@
-import Counter from "./Counter";
-import CounterHooks from "./CounterHooks";
-import React,{useState} from 'react';
-export const ThemeContext=React.createContext();
+import React from "react";
 
 function App() {
-  const [theme,setTheme]=useState('green')
-  return (
-    <ThemeContext.Provider value={{backgroundColor:theme}}>  
-    <h1>CounterHooks</h1>
-<CounterHooks initialCounter={0}/>
-<h1>Counter</h1>
-<Counter initialCounter={0}/>
-<button onClick={
-  ()=>setTheme(prevTheme=>(prevTheme==='red'?'blue':'red') )
-}>toggle</button>
-</ThemeContext.Provider>
-    );
+  return null;
 }
 
 export default App;
